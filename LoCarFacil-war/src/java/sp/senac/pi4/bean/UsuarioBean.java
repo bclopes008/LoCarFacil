4e8ac5041647386ed544f4dadb8fa1cc72923168
bruno.clopes <bruno.clopes@CAS419699W07D05.senacsp.edu.br>
2016-05-12 18:23:55 -0300

    Versão Inicial. Foi adicionado a parte do ejb e bean inicial do Usuario

---
+++ LoCarFacil-war/src/java/sp/senac/pi4/bean/UsuarioBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp.senac.pi4.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author bruno.clopes
+ */
+@ManagedBean
+@RequestScoped
+public class UsuarioBean {
+
+    /**
+     * Creates a new instance of UsuarioBean
+     */
+    public UsuarioBean() {
+    }
+    
+}

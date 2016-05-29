bd669bcae92e7868f3d8dc4e716a2e1105a31082
b_c_l <b_c_l@DESKTOP-TNH7R1B>
2016-05-29 00:06:50 -0300

    Adionado a parte do Aluguel. Telas e bean

---
+++ LoCarFacil-ejb/src/java/sp/senac/pi4/entidades/Aluguel.java
@@ -0,0 +1,219 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp.senac.pi4.entidades;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ *
+ * @author bruno.lopes
+ */
+public class Aluguel {
+    
+    private int id;
+    private String localizacaoRetirada;
+    private Date dataRetirada;
+    private String horaRetirada;
+    private String localizacaoDevolucao;
+    private Date dataDevolucao;
+    private String horaDevolucao;
+    private String grupo;
+    private boolean protecao;
+    private int bbConf = 0;
+    private int assElev = 0;
+    private int cadBB = 0;
+    private int qtdGps = 0;
+    private int dias;
+    private String selectAcessorios;
+    private BigDecimal valorBbConf;
+    private BigDecimal valorAssElev;
+    private BigDecimal valorCadBB;
+    private BigDecimal valorGps;
+    private BigDecimal valorTotal;
+    private BigDecimal valorGrupo;
+    private BigDecimal valTotalGrupo;
+
+    public Aluguel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocalizacaoRetirada() {
+        return localizacaoRetirada;
+    }
+
+    public void setLocalizacaoRetirada(String localizacaoRetirada) {
+        this.localizacaoRetirada = localizacaoRetirada;
+    }
+
+    public Date getDataRetirada() {
+        return dataRetirada;
+    }
+
+    public void setDataRetirada(Date dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+
+    public String getHoraRetirada() {
+        return horaRetirada;
+    }
+
+    public void setHoraRetirada(String horaRetirada) {
+        this.horaRetirada = horaRetirada;
+    }
+
+    public String getLocalizacaoDevolucao() {
+        return localizacaoDevolucao;
+    }
+
+    public void setLocalizacaoDevolucao(String localizacaoDevolucao) {
+        this.localizacaoDevolucao = localizacaoDevolucao;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public String getHoraDevolucao() {
+        return horaDevolucao;
+    }
+
+    public void setHoraDevolucao(String horaDevolucao) {
+        this.horaDevolucao = horaDevolucao;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public boolean getProtecao() {
+        return protecao;
+    }
+
+    public void setProtecao(boolean protecao) {
+        this.protecao = protecao;
+    }
+
+    public int getBbConf() {
+        return bbConf;
+    }
+
+    public void setBbConf(int bbConf) {
+        this.bbConf = bbConf;
+    }
+
+    public int getAssElev() {
+        return assElev;
+    }
+
+    public void setAssElev(int assElev) {
+        this.assElev = assElev;
+    }
+
+    public int getCadBB() {
+        return cadBB;
+    }
+
+    public void setCadBB(int cadBB) {
+        this.cadBB = cadBB;
+    }
+
+    public int getQtdGps() {
+        return qtdGps;
+    }
+
+    public void setQtdGps(int qtdGps) {
+        this.qtdGps = qtdGps;
+    }
+
+    public String getSelectAcessorios() {
+        return selectAcessorios;
+    }
+
+    public void setSelectAcessorios(String selectAcessorios) {
+        this.selectAcessorios = selectAcessorios;
+    }
+
+    public BigDecimal getValorBbConf() {
+        return valorBbConf;
+    }
+
+    public void setValorBbConf(BigDecimal valorBbConf) {
+        this.valorBbConf = valorBbConf;
+    }
+
+    public BigDecimal getValorAssElev() {
+        return valorAssElev;
+    }
+
+    public void setValorAssElev(BigDecimal valorAssElev) {
+        this.valorAssElev = valorAssElev;
+    }
+
+    public BigDecimal getValorCadBB() {
+        return valorCadBB;
+    }
+
+    public void setValorCadBB(BigDecimal valorCadBB) {
+        this.valorCadBB = valorCadBB;
+    }
+
+    public BigDecimal getValorGps() {
+        return valorGps;
+    }
+
+    public void setValorGps(BigDecimal valorGps) {
+        this.valorGps = valorGps;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public BigDecimal getValorGrupo() {
+        return valorGrupo;
+    }
+
+    public void setValorGrupo(BigDecimal valorGrupo) {
+        this.valorGrupo = valorGrupo;
+    }
+
+    public BigDecimal getValTotalGrupo() {
+        return valTotalGrupo;
+    }
+
+    public void setValTotalGrupo(BigDecimal valTotalGrupo) {
+        this.valTotalGrupo = valTotalGrupo;
+    }
+
+    public int getDias() {
+        return dias;
+    }
+
+    public void setDias(int dias) {
+        this.dias = dias;
+    }   
+    
+}
---
+++ LoCarFacil-war/src/java/sp/senac/pi4/bean/AluguelBean.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp.senac.pi4.bean;
+
+import sp.senac.pi4.entidades.Aluguel;
+import sp.senac.pi4.util.Mensagem;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+
+/**
+ *
+ * @author bruno.lopes
+ */
+@ManagedBean
+@SessionScoped
+public class AluguelBean {
+
+    private Aluguel aluguel;
+
+    public AluguelBean() {
+        aluguel = new Aluguel();
+    }
+
+    public Aluguel getAluguel() {
+        return aluguel;
+    }
+
+    public void setAluguel(Aluguel aluguel) {
+        this.aluguel = aluguel;
+    }
+
+    public String escolherGrupo() {
+        return "escolherGrupo";
+    }
+
+    public String carroSelecionado(String grupo, double valorGrupo) {
+        this.aluguel.setGrupo(grupo);
+        this.aluguel.setValorGrupo(new BigDecimal(valorGrupo));
+        //parei aqui
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+        Date data = aluguel.getDataRetirada();
+        String dtRt = fmt.format(data);
+        dtRt = dtRt.substring(0, 2);
+        int diaRet = Integer.parseInt(dtRt);
+        
+        data = aluguel.getDataDevolucao();
+        String dtDv = fmt.format(data);
+        dtDv = dtDv.substring(0, 2);
+        int diaDev = Integer.parseInt(dtDv);
+        this.aluguel.setDias((diaDev - diaRet) + 1);
+        resumo(0);
+        return "escolherGrupo.xhtml?faces-redirect=true";
+    }
+
+    public String protecao() {
+        if (aluguel.getGrupo() == null) {
+            // Mensagem de sucesso para usuário
+            Flash flash = FacesContext.getCurrentInstance()
+                    .getExternalContext().getFlash();
+            flash.put("mensagem", new Mensagem("erro", "error"));
+            return "escolherGrupo";
+        }
+        return resumo(0);
+    }
+
+    public String localData() {
+        return "localData";
+    }
+
+    public String resumo(int op) {
+        double assElev = aluguel.getAssElev() * 15 * aluguel.getDias();
+        double bbConf = aluguel.getBbConf() * 15 * aluguel.getDias();
+        double cadBb = aluguel.getCadBB() * 15 * aluguel.getDias();
+        double gps = aluguel.getQtdGps() * 15 * aluguel.getDias();
+        this.aluguel.setValorAssElev(new BigDecimal(assElev));
+        this.aluguel.setValorBbConf(new BigDecimal(bbConf));
+        this.aluguel.setValorCadBB(new BigDecimal(cadBb));
+        this.aluguel.setValorGps(new BigDecimal(gps));
+        //parei aqui
+        double vTotal = aluguel.getValorGrupo().doubleValue() * aluguel.getDias();
+        this.aluguel.setValTotalGrupo(new BigDecimal(vTotal));
+        
+        this.aluguel.setValorTotal(new BigDecimal(assElev + bbConf + cadBb + gps + vTotal));
+        System.err.println(aluguel.getValorTotal());
+        
+        //Número aleatório
+        Random gerador = new Random();
+        do{
+            this.aluguel.setId(gerador.nextInt());
+        }while (aluguel.getId() < 1);
+        if (op == 0) {
+            return "protecao.xhtml?faces-redirect=true";
+        } else {
+            return "resumo";
+        }
+    }
+
+    public String novaReserva() {
+        aluguel = new Aluguel();
+        return "localData";
+    }
+
+}
